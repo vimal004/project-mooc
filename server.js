6583c2e45b49c6163c0f96d26086f8fb5810cd10
vimal004 <2004.vimal@gmail.com>
2025-01-10 19:07:35 +0530

    feat: add root route to return a Hello World message

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.post("/gemini", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
